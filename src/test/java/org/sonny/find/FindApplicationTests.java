4e86c101babbdb41dfc7451d9adae20910e6abb4
Son <jongin718@gmail.com>
2024-07-27 19:22:26 +0900

    first commet

---
+++ src/test/java/org/sonny/find/FindApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonny.find;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
