24e93559379bba76a7912069398f0caeff5aa11c
son <jongin718@gmail.com>
2024-07-29 09:41:33 +0900

    Kakao API 관련 config, dto, service 파일 수정 및 추가

---
+++ src/main/java/org/sonny/find/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,24 @@
+package org.sonny.find.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService { // command + shift + T를 누르면 테스트 파일 생성 가능
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI buildUriByAddressSearch(String address) {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        uriBuilder.queryParam("query", address);
+
+        URI uri = uriBuilder.build().encode().toUri();
+        log.info("Kakao local search address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+}
